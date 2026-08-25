c731fe40bfb2091f8c78340920fb0defcbb07cdb
Mary Elizabeth Glassburner <glass.marybeth@gmail.com>
2026-08-25 10:58:40 -0400

    Add Fireflies

--- projects.js
+++ projects.js
@@ -1,3 +1,20 @@
+const container = document.getElementById("firefly-container");
+const NUMBER_OF_FIREFLIES = 18;
+for(let i = 0; i < NUMBER_OF_FIREFLIES; i++){
+    const firefly = document.createElement("div");
+    firefly.classList.add("firefly");
+    const size = Math.random() * 4 + 2;
+    firefly.style.width = `${size}px`;
+    firefly.style.height = `${size}px`;
+    firefly.style.left = `${Math.random()*100}vw`;
+    firefly.style.top = `${Math.random()*100}vh`;
+    firefly.style.animationDuration =
+        `${18 + Math.random()*18}s, ${3 + Math.random()*5}s`;
+    firefly.style.animationDelay =
+        `${Math.random()*10}s, ${Math.random()*5}s`;
+    container.appendChild(firefly);
+}
+
 const navbar = document.getElementById("navbar");
 const learnMore = document.getElementById("learnMore");
 const about = document.getElementById("about");
